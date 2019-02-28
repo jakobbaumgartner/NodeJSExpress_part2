d43100d77d1672edcd87ff4f50ca9fadf913df3e
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-02-28 23:28:44 +0100

    sendFile, path.join, __direname

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 const http = require('http')
 const express = require('express')
 const bpar = require('body-parser')
+// npm install body-parser
+
 
 const app = express()
+
+//importamo routes datoteke
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 app.use(bpar.urlencoded({extended: false}))
 //tako naredimo middleware, ki parse body in nato kliče next, na naslednjo funkcijo
 
-app.use('/newadded', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add !</button></form>')
-})
+app.use(adminRoutes)
+//uporabimo routes v routes files
 
-app.use('/product', (req, res, next) => {
-    console.log('productpage')
-    console.log(req.body)
-    res.redirect('/')
+app.use(shopRoutes)
+// app.use('/shop', shopRoutes)
+//"filteer" /shop nam omogoča, da so shopRoutes le strani, ki se začno z /shop/ ... recimo /shop/clothes
 
-    // npm install body-parser
-
-
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<p>First page!</p>')
-    console.log("firstpage")
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found! 404</h>')
+    //page not found status code 404 lahko ustvarimo tako, da ji določimo naslov '/', ki ga dobimo, ko ni nobena druga zahteva izpolnjena
+    //pred send lahko verižimo ukaze, recimo status in pa setHeader
+    console.log("404")
  
 })
+
+//app.get deluje podobno kot app.use le, da se sproži le pri get request
+//get req pomeni, da želi info
+//app.get()
+
+// app.post pa deluje kot app.use, sproži se na post request
+//post pomeni, da dobi server ( mi ) info
+// app.post() 
 
 app.listen(3000)
---
+++ routes/shop.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const path = require('path')
+//še eden od osnovnih elementov node.js
+
+const router = express.Router()
+
+//pomembno je, da uporabimo get metodo, da saj drugače preprečimo error 404
+
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+})
+
+// Če bi uporabili le /views/shop.html bi nodejs gledal v root directory na računalniku in ne bi našel datoteke
+// __direname (dva podčrtaja) nam poda pot do našega direktorija (posebna "spremelnjivka"), lahko bi napisali
+// __direname/views/shop.html, vendar pa uporabimo path.join zmožnost node.js, ki nam odpravi nesoglasje
+// datotečnega sistema linuxa in windowsa (en ima / in drugi \ neumno)
+// ../ nas da v parent directory
+
+
+module.exports = router
